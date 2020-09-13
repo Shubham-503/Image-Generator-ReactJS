8ec6fcb70a4362fbc89ee7bbe9e29b697c66c64f
Shubham <kr.shubham025@gmail.com>
2020-09-14 03:28:40 +0530

    changes uncontrolled to controlled event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
-  handleInputChange = (event) => {
-    console.log(event.target.value);
-  };
+  state = { term: "" };
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label htmlFor="">Image Search</label>
-            <input type="text" onChange={this.handleInputChange} />{" "}
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />{" "}
           </div>
         </form>
       </div>
