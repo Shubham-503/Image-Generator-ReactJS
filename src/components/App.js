761f7b70a85ad25dcaded6954b64cabd151b5094
Shubham <kr.shubham025@gmail.com>
2020-09-14 14:07:51 +0530

    images are accessible

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,29 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSubmitChange(term) {
-    console.log(term);
-    axios.get("https://api.unsplash.com/search/photos", {
+  // constructor(props) {
+  //   super();
+  //   this.onSubmitChange = this.onSubmitChange.bind(this);
+  // }
+  state = { images: [] };
+
+  onSubmitChange = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID h6yKHrseyNNS8mds_RKYAT8-M-r_MXaw5d0rSkX_Yxo",
       },
     });
-  }
+
+    console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSubmitChange} />
+        Found : {this.state.images.length} images
       </div>
     );
   }
